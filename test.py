509a6dea81172c04e88e97c733a2e3e1f290de62
Jeff McGinley <jeff.mcginley.indy@gmail.com>
2018-10-03 12:14:16 -0400

    initial revision

---
+++ test.py
@@ -0,0 +1,30 @@
+# not part of the project, just an example
+# of how to connect to and user SQLite
+
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE users (id int, username text, password text)"
+cursor.execute(create_table)
+
+user = (1, 'jose', 'asdf')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+users = [
+    (2, 'rolf', 'asdf'),
+    (3, 'anne', 'xyz')
+]
+
+cursor.executemany(insert_query, users)
+
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit()
+
+connection.close()
